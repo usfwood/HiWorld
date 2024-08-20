a9f7c7935f1eda5af4cb151d9e8e7be101edf265
usfwood <usfwood@gmail.com>
2024-08-20 01:39:56 -0400

    Updated hello-1.c and Makefile

--- hello-1.c
+++ hello-1.c
@@ -1,5 +1,6 @@
-/*  hello-1.c  The simplest kernel module.
- *  
+/*  hello-1.c  The simplest kernel module. 
+ *
+ *   *  
  *  Copyright (C) 2001 by Peter Jay Salzman
  * 
  *  08/02/2006  -- Updated by Rodirgo Rubira Branco
@@ -16,7 +17,7 @@
 
  int init_module(void)
  {
-     printk("<1>Hello World 1.\n");
+     printk(KERN_INFO "<1>Hello World 1.\n");
 
      // A non 0 return means init_module failed; module can't be loaded.
      return 0;
@@ -24,7 +25,7 @@
 
  void cleanup_module(void)
  {
-     printk(KERN_ALERT "Goodbye World 1.\n");
+     printk(KERN_INFO "Goodbye World 1.\n");
  }
 
 MODULE_LICENSE("GPL");
